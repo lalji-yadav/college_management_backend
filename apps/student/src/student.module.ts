8763f12e70a0eda5bbc745fc4837537ae7562a05
Lalji Yadav <anubhavya246@gmail.com>
2025-10-24 22:32:16 +0530

    add modules

--- apps/student/src/student.module.ts
+++ apps/student/src/student.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
 import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
+import { CoursesModule } from './courses/courses.module';
+import { OrdersModule } from './orders/orders.module';
+import { ContentsModule } from './contents/contents.module';
+import { StudentModule } from './student/student.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ProfileModule, CoursesModule, OrdersModule, ContentsModule, StudentModule, NotificationsModule],
   controllers: [StudentController],
   providers: [StudentService],
 })
